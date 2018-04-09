8a0c5e2e1eb00ddf7658f8e04746707ad963ecef
Abhijit Sinha <abhijit.sinha@delhivery.com>
2018-04-09 08:02:47 +0530

    chore: updated index

--- app/upload-drag/upload-drag.js
+++
@@ -1,5 +0,0 @@
-app.controller('Upload-Drag', ['$scope', '$http', function($scope, $http) {
-   $scope.imagePlaceholder = ['View 1','View 2','View 3','View 4','View 5', 'View 6'];
-   $scope.image = null;
-    $scope.imageFileName = '';
-}]);
